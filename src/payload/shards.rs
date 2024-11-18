6beaba3e735c84380b46cedf7f7f782527d03496
constlhq <const_lhq@126.com>
2024-11-18 16:37:52 +0800

    v0.1.1

--- src/payload/shards.rs
+++ src/payload/shards.rs
@@ -8,10 +8,30 @@ pub struct SplitShardPayload<'a> {
     pub split_key: &'a str,
 }
 
+impl<'a> SplitShardPayload<'a> {
+    pub fn new(shard_id: &'a str, split_key: &'a str) -> Self {
+        Self {
+            action: "split",
+            shard_id,
+            split_key,
+        }
+    }
+}
+
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct MergeShardPayload<'a> {
     pub action: &'a str,
     pub shard_id: &'a str,
     pub adjacent_shard_id: &'a str,
+}
+
+impl<'a> MergeShardPayload<'a> {
+    pub fn new(shard_id: &'a str, adjacent_shard_id: &'a str) -> Self {
+        Self {
+            action: "merge",
+            shard_id,
+            adjacent_shard_id,
+        }
+    }
 }
--- src/payload/topics.rs
+++ src/payload/topics.rs
@@ -15,10 +15,50 @@ pub struct CreateTopicPayload<'a> {
     pub expand_mode: Option<&'a str>,
 }
 
+impl<'a> CreateTopicPayload<'a> {
+    pub fn new_blob_topic(shard_count: u32, lifecycle: u32, comment: &'a str) -> Self {
+        Self {
+            action: "create",
+            shard_count,
+            lifecycle,
+            record_type: RecordType::BLOB,
+            record_schema: None,
+            comment,
+            expand_mode: None,
+        }
+    }
+    pub fn new_tuple_topic(
+        shard_count: u32,
+        lifecycle: u32,
+        record_schema: &'a str,
+        comment: &'a str,
+    ) -> Self {
+        Self {
+            action: "create",
+            shard_count,
+            lifecycle,
+            record_type: RecordType::TUPLE,
+            record_schema: Some(record_schema),
+            comment,
+            expand_mode: None,
+        }
+    }
+}
+
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct AppendFieldPayload<'a> {
     pub action: &'a str,
     pub field_name: &'a str,
     pub field_type: FieldType,
+}
+
+impl<'a> AppendFieldPayload<'a> {
+    pub fn new(field_name: &'a str, field_type: FieldType) -> Self {
+        Self {
+            action: "appendfield",
+            field_name,
+            field_type,
+        }
+    }
 }
